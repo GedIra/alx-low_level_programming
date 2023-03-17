362619c21972f465aeb176b1edd93a6aef144ece
GedIra <irankundag65@gmail.com>
2023-03-17 14:24:34 -0700

    maybe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int g;
 	float f;
 
-printf("Size of a char: %zu byte(s)", (x))
-printf("size of an int: %zu byte(s)", (n))
-printf("Size of a long int: %zu byte(s)", (l))
-printf("Size of a long long int: %zu byte(s)", (g))
-printf("size of a float: %zu byte(s)", (f))
+printf("Size of a char: %zu byte(s)", sizeof(x))
+printf("size of an int: %zu byte(s)", sizeof(n))
+printf("Size of a long int: %zu byte(s)", sizeof(l))
+printf("Size of a long long int: %zu byte(s)", sizeof(g))
+printf("size of a float: %zu byte(s)", sizeof(f))
 return (0)
 }
