fd4e4bf45db8bbac53a1662f81c9935f1ece5b1e
GedIra <irankundag65@gmail.com>
2023-03-17 13:15:59 -0700

    basii

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
+#include <stdio.h>
 /**
  * main -AS code writes the text in puts as it is
  *return: 0 (success)
  */
-#include <stdio.h>
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
