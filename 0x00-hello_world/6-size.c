563f679c6ab612186583ede8cb405eb8d4b707ed
GedIra <irankundag65@gmail.com>
2023-03-18 03:49:45 -0700

    todat

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
-*Main - a C program that prints the size of various types on the computer it is compiled and run on.
+*main - a C program that prints the size of various types on the computer it is compiled and run on.
 *Return: 0 (success)
 */
 int main(void)
 {
-	char x;
-	int n;
-	long int l;
-	long long int g;
-	float f;
-
+char x;
+int n;
+long int l;
+long long int g;
+float f;
 printf("Size of a char: %zu byte(s)\n", sizeof(x));
 printf("size of an int: %zu byte(s)\n", sizeof(n));
 printf("Size of a long int: %zu byte(s)\n", sizeof(l));
