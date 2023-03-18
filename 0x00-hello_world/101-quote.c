cfa66aad1dd5d45b84342d6e1f7476f13d6bf94e
GedIra <irankundag65@gmail.com>
2023-03-18 06:12:28 -0700

    yup

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+* main - Write a C program that prints exactly what is in write function
+* Return : 1 (Always)
+*/
+int main(void)
+{
+iwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
 *main - a C program that prints the size of various types.
-*Return: 0 (success)
+*Return : 0 (success)
 */
 int main(void)
 {
-char x;
-int n;
-long int l;
-long long int g;
-float f;
-printf("Size of a char: %zu byte(s)\n", sizeof(x));
-printf("size of an int: %zu byte(s)\n", sizeof(n));
-printf("Size of a long int: %zu byte(s)\n", sizeof(l));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(g));
-printf("size of a float: %zu byte(s)\n" sizeof(f));
-return (0);
+	int a;
+	char x;
+	long int l;
+	long long int o;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(o));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
