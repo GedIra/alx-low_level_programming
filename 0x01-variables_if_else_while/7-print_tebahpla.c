450bc788a6702242e55ecbe2f4086e5cf6bf3d62
GedIra <irankundag65@gmail.com>
2023-03-20 16:48:42 -0700

    remaing

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints the lowercase alphabet from z to a
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
