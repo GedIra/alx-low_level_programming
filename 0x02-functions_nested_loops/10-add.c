2501705c45311b3760c40ddeda4ca03d6854be8e
GedIra <irankundag65@gmail.com>
2023-03-22 13:14:58 -0700

    faild

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two numbers.
+ * @b: a number to be added.
+ * Return: sum of a and b.
+ */
+int add(int a , int b)
+{
+	int c;
+	return (c);
+}
+
